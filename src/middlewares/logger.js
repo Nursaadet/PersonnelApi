9a902546e626dd7d9e1457d5ae8aa5572187ae5f
Nursaadet <nursaadetozdemir@gmail.com>
2026-02-13 18:15:04 +0300

    git add . git commit -m "fix logger for production environment" git push

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,39 +1,28 @@
 "use strict";
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
-// LOGGER
-// npm i morgan
-// https://expressjs.com/en/resources/middleware/morgan.html
 
-const morgan = require('morgan')
+const morgan = require("morgan");
+const fs = require("node:fs");
+const path = require("node:path");
 
-// app.use(morgan('tiny'))
-// app.use(morgan('short'))
-// app.use(morgan('dev'))
-// app.use(morgan('common'))
-// app.use(morgan('combined'))
-// Custom Log:
-// app.use(morgan('TIME=":date[iso]" - URL=":url" - Method=":method" - IP=":remote-addr" - Ref=":referrer" - Status=":status" - Sign=":user-agent" (:response-time[digits] ms)'))
+if (process.env.NODE_ENV === "production") {
+  // Production (Render) → console log
+  module.exports = morgan("combined");
+} else {
+  // Local development → file log
 
-// Write to File:
-// const fs = require('node:fs')
-// app.use(morgan('combined', {
-//     stream: fs.createWriteStream('./access.log', { flags: 'a+' })
-// }))
+  const logDir = path.join(__dirname, "../../logs");
 
-// Write to File - Day by day:
-const fs = require('node:fs')
-const now = new Date()
-// console.log(now, typeof now)
-const today = now.toISOString().split('T')[0]
-// console.log(today, typeof today)
-// app.use(morgan('combined', {
-//     stream: fs.createWriteStream(`./logs/${today}.log`, { flags: 'a+' })
-// }))
+  if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir, { recursive: true });
+  }
 
-module.exports = morgan('combined', {
-    stream: fs.createWriteStream(`./logs/${today}.log`, { flags: 'a+' })
-})
+  const today = new Date().toISOString().split("T")[0];
 
-/* ------------------------------------------------------- */
+  module.exports = morgan("combined", {
+    stream: fs.createWriteStream(
+      path.join(logDir, `${today}.log`),
+      { flags: "a+" }
+    ),
+  });
+}
+
